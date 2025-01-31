35748375fc479f9f378d8efc6f6547973a0dd664
LeonardoT-V <iderltv12@gmail.com>
2025-01-31 16:59:25 -0500

    add next-auth provider

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,7 +1,14 @@
+"use client";
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/sonner";
 const queryClient = new QueryClient();
+
+export default function Providers({ children }) {
+  return (
+    <>
+      <SessionProvider>
         <QueryClientProvider client={queryClient}>
           <ThemeProvider
             attribute="class"
@@ -14,3 +21,7 @@ const queryClient = new QueryClient();
             {children}
           </ThemeProvider>
         </QueryClientProvider>
+      </SessionProvider>
+    </>
+  );
+}
