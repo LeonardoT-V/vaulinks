68c10b253b4341f0a3079194911ace964b82ed32
LeonardoT-V <iderltv12@gmail.com>
2025-01-31 16:56:12 -0500

    add theme provider

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,2 +1,12 @@
+import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
             <Toaster />
+
+            {children}
+          </ThemeProvider>
