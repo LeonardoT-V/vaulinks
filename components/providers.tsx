d376c63b24fd5fd4aa2904a3f97216b386924503
LeonardoT-V <iderltv12@gmail.com>
2025-02-26 23:08:27 -0500

    default theme in app

--- components/providers.tsx
+++ components/providers.tsx
@@ -12,8 +12,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         <QueryClientProvider client={queryClient}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
-            enableSystem
+            defaultTheme="dark"
             disableTransitionOnChange
           >
             <Toaster />
