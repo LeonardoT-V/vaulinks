cf7a71d959b7fed82d49651d5e13786287b5c3d7
LeonardoT-V <iderltv12@gmail.com>
2025-02-26 23:08:10 -0500

    type provider

--- components/providers.tsx
+++ components/providers.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/sonner";
 const queryClient = new QueryClient();
 
-export default function Providers({ children }) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
       <SessionProvider>
