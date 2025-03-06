b4aa0328b63536f02a6ca7aeebe8b205fc930c12
LeonardoT-V <iderltv12@gmail.com>
2025-03-05 22:46:28 -0500

    configure font in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,10 @@ export default {
   ],
   theme: {
   	extend: {
+      fontFamily: {
+        sans: "'Geist Variable', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+        mono: ["Geist Mono Variable", "monospace"],
+      },
   		colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
