ec4d476bcd5a4ab372df4be8472cf7e586367612
LeonardoT-V <iderltv12@gmail.com>
2025-03-05 22:50:13 -0500

    add login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,24 @@
-import React from "react";
-
-import { signIn } from "@/auth";
-
-export function SignIn() {
-  return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("github");
-      }}
-    >
-      <button type="submit">Signin with GitHub</button>
-    </form>
-  );
-}
+import { APP_ICON_JSX, APP_NAME } from "@/const";
+import { LoginForm } from "@/components/login/login-form";
 
 export default function LoginPage() {
-  return <SignIn />;
+  return (
+    <div className="grid min-h-svh w-full lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium text-xl">
+            <div className="flex h-7 w-7 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <APP_ICON_JSX className="size-6" />
+            </div>
+            {APP_NAME}
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
