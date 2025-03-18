6a18a462412324b9c2a79ae46fcb374244e80e69
LeonardoT-V <iderltv12@gmail.com>
2025-03-18 12:08:31 -0500

    hooks for manage boomarks

--- hooks/use-bookmarks.tsx
+++ hooks/use-bookmarks.tsx
@@ -1,5 +1,6 @@
-import { createBookmark } from "@/actions/bookmarks";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createBookmark, findMyBookmark } from "@/actions/bookmarks";
+import { useMutation, useQueryClient, useQuery } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
 
 // export function useCreateFolderForm() {
 //     return useForm<z.infer<typeof createFolderSchema>>({
@@ -12,6 +13,20 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 //     });
 //   }
 
+const BOOKMARK_QUERY_KEY = {
+  myBookmark: "myBookmark",
+};
+
+export function useGetBookmarks() {
+  const { data: session } = useSession();
+  const user = session?.user;
+  return useQuery({
+    queryKey: [BOOKMARK_QUERY_KEY.myBookmark],
+    queryFn: () => findMyBookmark(user?.id || ""),
+    enabled: !!user,
+  });
+}
+
 export function validateBookmark(form) {
   console.log(form);
 }
@@ -19,8 +34,15 @@ export function validateBookmark(form) {
 export function useCreateBookmark() {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: async (bookmark) => await createBookmark(bookmark),
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["getUsers"] }),
+    mutationFn: async (bookmark: {
+      title: string;
+      url: string;
+      userId: string;
+    }) => await createBookmark(bookmark),
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: [BOOKMARK_QUERY_KEY.myBookmark],
+      }),
     onError: (error) => console.log("error"),
   });
 }
